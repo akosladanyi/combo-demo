912d115d8e1cdd99368d4d2e2b009515da64f57a
Akos Ladanyi <akos.ladanyi@gmail.com>
2015-11-22 14:22:53 +0100

    Rundaemon used in HPSR demo

--- combosite/main/management/commands/rundaemon.py
+++ combosite/main/management/commands/rundaemon.py
@@ -5,24 +5,47 @@ from time import sleep
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        net_lst = list(Network.objects.all())
+        net1 = Network.objects.get(pk=1)
+        net2 = Network.objects.get(pk=2)
+        LIMIT = 2000
         while True:
-            for net in Network.objects.all():
-                cl_lst = list(net.client_set.order_by('id').all())
-                if len(cl_lst) > 1:
-                    sum_spd = 0.0
-                    for cl in cl_lst:
-                        sum_spd += cl.dl_speed
-                    if sum_spd > 3500:
-                        cl = cl_lst[0]
-                        try:
-                            i = net_lst.index(cl.network)
-                        except ValueError:
-                            continue
-                        net = net_lst[(i + 1) % len(net_lst)]
-                        cl.network = net
-                        cl.save()
-                        self.stdout.write('Client {} new network: {}'.format(
-                            cl, net))
-                        break
+            clients1 = list(Client.objects.filter(network=net1).order_by('id'))
+            clients2 = list(Client.objects.filter(network=net2).order_by('id'))
+            cl1 = Client.objects.get(pk=1)
+            cl2 = Client.objects.get(pk=2)
+            trf1 = 0.0
+            trf2 = 0.0
+            for cl in clients1:
+                trf1 += cl.dl_speed
+            for cl in clients2:
+                trf2 += cl.dl_speed
+            self.stdout.write('{}/{} {}/{}'.format(len(clients1), len(clients2), trf1, trf2))
+            # # if len(clients1) > 1 and trf1 > LIMIT and cl2.dl_speed > LIMIT:
+            # if len(clients1) > 1 and trf1 > LIMIT:
+            #     cl = clients1[0]
+            #     cl.network = net2
+            #     cl.save()
+            #     self.stdout.write('Client {} new network: {}'.format(cl, net2))
+            # elif trf1 < LIMIT and len(clients2) > 0:
+            #     cl = clients2[0]
+            #     cl.network = net1
+            #     cl.save()
+            #     self.stdout.write('Client {} new network: {}'.format(cl, net1))
+            if cl1.network == net1 and cl2.network == net1 and cl2.dl_speed > LIMIT:
+                cl1.network = net2
+                cl1.save()
+                self.stdout.write('Client {} new network: {}'.format(cl1, net2))
+            elif cl1.network == net2 and cl2.network == net2 and cl2.dl_speed > LIMIT:
+                cl1.network = net1
+                cl1.save()
+                self.stdout.write('Client {} new network: {}'.format(cl1, net1))
+            elif cl1.network == net1 and cl2.network == net2 and cl2.dl_speed < LIMIT:
+                cl1.network = net2
+                cl1.save()
+                self.stdout.write('Client {} new network: {}'.format(cl1, net2))
+            elif cl1.network == net2 and cl2.network == net1 and cl2.dl_speed < LIMIT:
+                cl1.network = net1
+                cl1.save()
+                self.stdout.write('Client {} new network: {}'.format(cl1, net1))
+
             sleep(5)
