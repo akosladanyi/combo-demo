243f1869081001cf6187a67e5e782454fc106c2e
Akos Ladanyi <akos.ladanyi@gmail.com>
2015-11-22 14:17:16 +0100

    Admin for client model

--- combosite/main/admin.py
+++ combosite/main/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from main.models import Network, Client
 
 
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'network']
+
+
 admin.site.register(Network)
-admin.site.register(Client)
+admin.site.register(Client, ClientAdmin)
